45a22bfb4c0069ccaf676ba6205bb5579bfd5ce5
nitin <nitingoley21@gmail.com>
2025-01-27 11:17:52 +0530

    Recording Page done successfully

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,26 @@
 import { clsx, type ClassValue } from "clsx"
+import { intervalToDuration } from "date-fns";
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+
+
+export const calculatedRecordingTimeDuration = (startTime:string , endTime:string)=>{
+  const start = new Date(startTime);
+  const end = new Date(endTime);
+
+  const duration = intervalToDuration({start , end});
+
+  if(duration.hours && duration.hours > 0) {
+    return `${duration.hours}:${String(duration.minutes).padStart(2, "0")}:${String(duration.seconds).padStart(2, "0")}`;
+  }
+
+  if(duration.minutes && duration.minutes > 0) {
+    return `${duration.minutes}:${String(duration.seconds).padStart(2, "0")}`;
+  }
+
+  return `${duration.seconds} seconds`;
 }
